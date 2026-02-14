24dbc48c93976d7e90facbabf0413466c82f16ec
IfedaForage <origincod3@gmail.com>
2026-02-14 16:30:44 -0500

    Changes Made
    NewBehaviorScript Made

---
+++ Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
